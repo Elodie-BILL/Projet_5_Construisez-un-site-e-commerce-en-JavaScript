b88a3ce3f9b548fe553b39b4cac2c6476dfab4ee
Elodie B <billelodie@gmail.com>
2022-09-13 15:42:58 +0200

    first commit:page d'accueil en cours

---
+++ front/js/script.js
@@ -0,0 +1,36 @@
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function (products){
+        // console.log(products);
+         for ( let product of products) {
+            
+            
+            // Récupérer l'élèment HTML grâce à son id
+            
+            // Insérer un produit en reprenant le template en commentaire de la page index.html avec string Interpolation
+        
+            let sectionItems = document.getElementById('items');
+            console.log(sectionItems);
+        
+            let displayProduct = ` <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription"> ${product.description}</p>
+            </article>
+          </a> `
+
+          sectionItems.innerHTML+= displayProduct
+        }  
+           
+            
+    })
+    
+    .catch(function (error) {
+        // Une erreur est survenue
+        console.log(error)
+    })
