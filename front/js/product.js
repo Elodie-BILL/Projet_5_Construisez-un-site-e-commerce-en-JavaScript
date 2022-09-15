4269411e25fd02b8e0c9ac0c0ba3e09b9c8bbd1b
Elodie B <billelodie@gmail.com>
2022-09-15 16:51:38 +0200

    nouvelle tentative de mise en ligne produit

--- front/js/product.js
+++ front/js/product.js
@@ -9,17 +9,18 @@ console.log (urlKanapSinope);*/
 // * product's details
 // * picture
 
-//essai 1
+//test 1:
+
 /*let sinopePic = document.getElementsByClassName('item__img');
 let sinopeKanap = `img src="${product.imageUrl}" alt="${product.altTxt}"`;
 console.log(sinopeKanap)*/
 
-//essai 2
+//test 2:
 // let sinopePic = document.getElementsByClassName('item__img'); 
 // let sinopeKanap = ' <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d\'un canapé bleu, deux places" >';
 // console.log(sinopeKanap)
 
-//essai 3
+//test 3:
 fetch("http://localhost:3000/api/products")
     .then(function (res) {
         if (res.ok) {
@@ -28,9 +29,9 @@ fetch("http://localhost:3000/api/products")
     })
     .then(function (products){
         for ( let product of products) {
-            let sinopePic = document.getElementsByClassName('item__img'); 
-            let sinopeKanap = ' <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d\'un canapé bleu, deux places" >';
-            sinopePic.innerHTML+= sinopeKanap
+            // let sinopePic = document.getElementsByClassName('item__img'); 
+            // let sinopeKanap = ' <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d\'un canapé bleu, deux places" >';
+            // sinopePic.innerHTML+= sinopeKanap
         }  
         
     })
@@ -38,9 +39,47 @@ fetch("http://localhost:3000/api/products")
 
             // Une erreur est survenue
             console.log(error)
-        })    
+        })/*    
 
         // On verra plus tard... :'(
+
+        // reprise*/
+//
+
+//test 4:
+/*fetch("http://localhost:3000/api/products")
+.then(function (res) {
+    if (res.ok) {
+        return res.json();
+    }
+})
+let imageUrl="http://localhost:3000/images/kanap01.jpeg"
+const sinopePic = document.createElement(img);
+sinopePic.src= imageUrl;*/
+
+//test 5: 
+// Je récupère les infos de l'API + je sélectionne celles dont j'ai besoin et je l'affiche sur la page
+// fetch("http://localhost:3000/api/products")
+// .then(function (res) {
+//     if (res.ok) {
+//         return res.json();
+//     }
+// })
+// let sinopePic = document.getElementsByClassName('item__img');
+// let sinopeKanap = `img src="${product.imageUrl}" alt="${product.altTxt}"`;
+
+const idKanapSinope = window.location.search.split("?id=").join();
+console.log(idKanapSinope);
+// let imageUrl= 'http://localhost:3000/images/kanap01.jpeg';
+// let altTxt = 'Photo d\'un canapé bleu, deux places';
+
+const sinopePic = document.getElementsByClassName('item__img');
+console.log(sinopePic);
+
+let displaySinopePic = `<img src="${imageUrl}" alt="${altTxt}"></img>`;
+sinopePic.innerHTML+=displaySinopePic;
+console.log(displaySinopePic)
+
 
     
 
