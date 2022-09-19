96447eb10c5c31a86b34bd2230b9e72493529eeb
Elodie B <billelodie@gmail.com>
2022-09-19 21:41:27 +0200

    même chose / code nettoyer

--- front/js/product.js
+++ front/js/product.js
@@ -1,94 +1,47 @@
 
-//récupération du produit à afficher
-// first product : 'Kanap Sinopé'
+
+
+// Recupération id dans l'url
 let urlKanapSinope = document.location.href; 
 let urlproduct = new URL(urlKanapSinope);
 let idKanap = urlproduct.searchParams.get("id");
 console.log(idKanap);
 
 
-// * product's details
-// * picture
-
-//test 1:
-
-/*;
-console.log(sinopeKanap)*/
-
-//test 2:
-// let sinopePic = document.getElementsByClassName('item__img'); 
-// let sinopeKanap = ' <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d\'un canapé bleu, deux places" >';
-// console.log(sinopeKanap)
-
-//test 3:
+//Récupération des infos produits
 fetch(`http://localhost:3000/api/products/${idKanap}`)
-    .then(function (res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function (product){
-        let sinopePic = document.getElementsByClassName('item__img');
-        let displaySinopePic = `<img src="${imageUrl}" alt="${altTxt}"/>`;
-
-        sinopePic.innerHTML = displaySinopePic;
-        // for ( let product of products) {
-        //     // let sinopePic = document.getElementsByClassName('item__img'); 
-        //     // let sinopeKanap = ' <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d\'un canapé bleu, deux places" >';
-        //     // sinopePic.innerHTML+= sinopeKanap
-        // }  
-        console.log(product)
-    })
-    .catch(function (error) {
-
-            // Une erreur est survenue
-            console.log(error)
-        })   
-
-        // On verra plus tard... :'(
-
-        // reprise
-//
-
-//test 4:
-/*fetch("http://localhost:3000/api/products")
 .then(function (res) {
     if (res.ok) {
         return res.json();
     }
 })
-let imageUrl="http://localhost:3000/images/kanap01.jpeg"
-const sinopePic = document.createElement(img);
-sinopePic.src= imageUrl;*/
+.then(function (product){
+    let kanapPic = document.getElementsByClassName('item__img');
 
-//test 5: 
-// Je récupère les infos de l'API + je sélectionne celles dont j'ai besoin et je l'affiche sur la page
-// fetch("http://localhost:3000/api/products")
-// .then(function (res) {
-//     if (res.ok) {
-//         return res.json();
-//     }
-// })
-// let sinopePic = document.getElementsByClassName('item__img');
-// let sinopeKanap = `img src="${product.imageUrl}" alt="${product.altTxt}"`;
+    let displaySinopePic = `<img src="${imageUrl}" alt="${altTxt}"/>`;
 
-const idKanapSinope = window.location.search.split("?id=").join("");
+   kanapPic.innerHTML = displaySinopePic;
+ 
+    console.log(product);
+})
+.catch(function (error) {
+    // let error = "Une erreur est survenue";
+    console.log(error)
+})  
 
-console.log(idKanapSinope);
+//Intégration information dans page produit
 
 const kanapSinope = async () => {
-       fetch ("http:localhost:3000/api/products/${idKanapSinope}")
-        .then((res) => res.json())
-        .then((promise) => {
-            console.log(promise);
-        });
+    fetch ("http:localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((promise) => {
+        console.log(promise);
+    });
 
 };
 
 console.log(kanapSinope);
 
-// let imageUrl= 'http://localhost:3000/images/kanap01.jpeg';
-// let altTxt = 'Photo d\'un canapé bleu, deux places';
 
 
 const imageUrl = async () => {
@@ -101,7 +54,7 @@ const imageUrl = async () => {
 
 
 const altTxt = async () => {
-    fetch ("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926".altTXT)
+    fetch (`http://localhost:3000/api/products/${idKanap}.altTXT`)
     .then ((res) => res.json())
     .then((promise) => {
         console.log(promise);            
@@ -111,9 +64,11 @@ const altTxt = async () => {
 const sinopePic = document.getElementsByClassName('item__img');
 console.log(sinopePic);
 
+// Affichage info
 
+let itemsdescription = document.getElementById(description)
+itemsdDescription.innerHTML = window.description;                                                                                                                                                                                                                          
 
-// console.log(displaySinopePic)
 
 
     
