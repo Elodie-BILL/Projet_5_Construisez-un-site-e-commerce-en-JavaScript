d1b7e4e96ce4b35f25c131acc7d6b39856ead414
Elodie B <billelodie@gmail.com>
2022-09-19 13:05:28 +0200

    Récupération URL et id + mise en console infos produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,10 @@
 
 //récupération du produit à afficher
-/*first product : 'Kanap Sinopé'
-let urlKanapSinope = '107fb5b75607497b96722bda5b504926';
-let urlOnLigne = new URLSearchParams(urlKanapSinope);
-console.log (urlKanapSinope);*/
+// first product : 'Kanap Sinopé'
+let urlKanapSinope = document.location.href; 
+let urlproduct = new URL(urlKanapSinope);
+let idKanap = urlproduct.searchParams.get("id");
+console.log(idKanap);
 
 
 // * product's details
@@ -11,8 +12,7 @@ console.log (urlKanapSinope);*/
 
 //test 1:
 
-/*let sinopePic = document.getElementsByClassName('item__img');
-let sinopeKanap = `img src="${product.imageUrl}" alt="${product.altTxt}"`;
+/*;
 console.log(sinopeKanap)*/
 
 //test 2:
@@ -21,29 +21,33 @@ console.log(sinopeKanap)*/
 // console.log(sinopeKanap)
 
 //test 3:
-fetch("http://localhost:3000/api/products")
+fetch(`http://localhost:3000/api/products/${idKanap}`)
     .then(function (res) {
         if (res.ok) {
             return res.json();
         }
     })
-    .then(function (products){
-        for ( let product of products) {
-            // let sinopePic = document.getElementsByClassName('item__img'); 
-            // let sinopeKanap = ' <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d\'un canapé bleu, deux places" >';
-            // sinopePic.innerHTML+= sinopeKanap
-        }  
-        
+    .then(function (product){
+        let sinopePic = document.getElementsByClassName('item__img');
+        let displaySinopePic = `<img src="${imageUrl}" alt="${altTxt}"/>`;
+
+        sinopePic.innerHTML = displaySinopePic;
+        // for ( let product of products) {
+        //     // let sinopePic = document.getElementsByClassName('item__img'); 
+        //     // let sinopeKanap = ' <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d\'un canapé bleu, deux places" >';
+        //     // sinopePic.innerHTML+= sinopeKanap
+        // }  
+        console.log(product)
     })
     .catch(function (error) {
 
             // Une erreur est survenue
             console.log(error)
-        })/*    
+        })   
 
         // On verra plus tard... :'(
 
-        // reprise*/
+        // reprise
 //
 
 //test 4:
@@ -68,17 +72,48 @@ sinopePic.src= imageUrl;*/
 // let sinopePic = document.getElementsByClassName('item__img');
 // let sinopeKanap = `img src="${product.imageUrl}" alt="${product.altTxt}"`;
 
-const idKanapSinope = window.location.search.split("?id=").join();
+const idKanapSinope = window.location.search.split("?id=").join("");
+
 console.log(idKanapSinope);
+
+const kanapSinope = async () => {
+       fetch ("http:localhost:3000/api/products/${idKanapSinope}")
+        .then((res) => res.json())
+        .then((promise) => {
+            console.log(promise);
+        });
+
+};
+
+console.log(kanapSinope);
+
 // let imageUrl= 'http://localhost:3000/images/kanap01.jpeg';
 // let altTxt = 'Photo d\'un canapé bleu, deux places';
+
+
+const imageUrl = async () => {
+        fetch ("http://localhost:3000/images/kanap01.jpeg")
+        .then ((res) => res.json())
+        .then((promise) => {
+            console.log(promise);            
+        });
+};
+
+
+const altTxt = async () => {
+    fetch ("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926".altTXT)
+    .then ((res) => res.json())
+    .then((promise) => {
+        console.log(promise);            
+    });
+};
 
 const sinopePic = document.getElementsByClassName('item__img');
 console.log(sinopePic);
 
-let displaySinopePic = `<img src="${imageUrl}" alt="${altTxt}"></img>`;
-sinopePic.innerHTML+=displaySinopePic;
-console.log(displaySinopePic)
+
+
+// console.log(displaySinopePic)
 
 
     
