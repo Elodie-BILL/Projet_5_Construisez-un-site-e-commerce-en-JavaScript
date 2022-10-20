c5a8857b60beb27f10ec9088ce6234c3b23f7884
Elodie B <billelodie@gmail.com>
2022-10-20 14:58:07 +0200

    suite travaille sur affichage produit dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,72 +1,104 @@
 let cart = JSON.parse(localStorage.getItem('produit'));
 console.log(cart);
 
+let cartHtmlCode = document.getElementById('cart__items');
+// console.log(cartHtmlCode);
+  
+let displayCart = '';
 
+for ( product of cart){
+  //console.log(product)  
+  let productId = product._id;
+  let productColor = product.colors;
+  let productQuantity = product.quantity;
+    
+  displayCart = displayCart + ` <article class="cart__item" data-id=${productId} data-color="${productColor}">
+          <div class="cart__item__img">
+              <img src="" alt="Photographie d'un canapé">
+            </div>
+            <div class="cart__item__content">
+              <div class="cart__item__content__description">
+                <h2> </h2>
+                <p>${productColor}</p>
+                <p> </p>
+              </div>
+              <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                  <p>Qté : </p>
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productQuantity}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                  <p class="deleteItem">Supprimer</p>
+                </div>
+              </div>
+            </div>
+            </article>`;
+  cartHtmlCode.innerHTML= displayCart;
+};
 
 fetch(`http://localhost:3000/api/products`)
-  
   .then(function(response){
-    if (response){
-      return response.json();
-     
-    }      
+      if (response){
+        return response.json();
+          
+      }      
   })
-
   .then(function (products){
-    // console.log(products)
+    console.log(products);
 
-    for (let article of products){
-      console.log(article);
-      
-    }
-    // let data = document.getElementsByClassName('cart__item__content__description');
-    // console.log(data);
-        
-  })
-
-  .then((product)=>{
-    let cartHtmlCode = document.getElementById('cart__items');
-    // console.log(cartHtmlCode);
-
-    let displayCart = ''; 
-
-    for ( product of cart){
-      //console.log(product);
-
-      displayCart = displayCart + ` <article class="cart__item" data-id=${product._id} data-color="${product.colors}">
-      <div class="cart__item__img">
-          <img src="" alt="Photographie d'un canapé">
-        </div>
-        <div class="cart__item__content">
-          <div class="cart__item__content__description">
-            <h2>Nom du produit</h2>
-            <p>Vert</p>
-            <p>42,00 €</p>
-          </div>
-          <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-              <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-            </div>
-            <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
-            </div>
-          </div>
-        </div>
-        </article>`;
-
-      cartHtmlCode.innerHTML= displayCart;
-
-      // console.log(displayCart);
+    products.forEach(element => console.log(element));
+    let o = element[0];
+    console.log(o);
+   
+    //  for ( let article of products){
+    //     console.log(article);
+    //     let code = document.getElementById('cart__items').getElementsByClassName('cart__item__content__description');
+    //     console.log(code);
+    //     let codeEdited = `<div class="cart__item__content__description">
+    //     <h2>${article.name} </h2>
+    //     <p>${productColor}</p>
+    //     <p> ${article.price}</p>
+    //     </div>`
+    //     code.innerHTML=codeEdited;
+    //   }
     
-    }
-  })
+
+                
   
-    
-    .catch(function (error) {
-      
-      console.log("une erreur est survenue");
   })
+  .catch(function (error) {
+    console.log("une erreur est survenue");
+  });
+//
 
-// trouve solution pour récupérer les infos dans la liste de produits en fonctiond 
-// e l'ide du produit qui est dans le panier à vhaque tour de boucle
+
+
+
+
+
+
+
+
+
+
+
+// console.log(displayCart);
+
+    // Je veux selectionner les informations des articles dans products et
+    //  les injecter dans le code HTML en fonction de _id déjà dans le panier
+
+    // let apiData = document.getElementsByClassName('cart__item__content__description');
+    // console.log(apiData);
+
+    // for (article of products){
+    //   // console.log(products);
+    //   let price = article.price;
+    //   let name = article.name;
+    //   let picture = article.imageUrl;
+    //   console.log(picture);
+
+    // }
+    
+
+    // let article = products;
+    // console.log(article);
