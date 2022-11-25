4a600da67a049d64522f82645b2ea8309d74c98e
Elodie B <billelodie@gmail.com>
2022-11-25 19:50:29 +0100

    Tentative code suppression ID

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -8,6 +8,15 @@ function orderNumber(){
         return orderIdNum;
 };
 
+function deleteOrderNumber(){
+    const urlCart = document.location.href;
+    const urlConfirme = new URL(urlCart);
+    const orderIdNum = urlConfirme.searchParams.get("id");
+    
+    console.log(orderIdNum);
+        return orderIdNum;
+};
+
 
 // Afficher numéro de commande 
 let confirmNumber = document.getElementById("orderId");
@@ -17,6 +26,25 @@ confirmNumber.innerText = `${orderNumber()}`;
 //Suppression panier
 const deleteCart = localStorage.clear('produit');
 console.log(JSON.parse(localStorage.getItem('produit')));
+
+
+// Supression numéro de commande
+// window.location.reload();
+deleteOrderNumber();
+
+// if( closeWindow() ){ 
+//     alert = ('Les informations sur cette page vont être supprimées. Etes-vous de vouloir quittez?');
+//     const urlCart = document.location.href;
+//     console.log(urlCart);
+//     const urlConfirme = new URL(urlCart);
+//     const orderIdNum = '';
+//         return orderIdNum;
+
+// }
+
+// function closeWindow () { 
+//     window.addEventListener("beforeunload", (event) =>{})
+// };  
 
 
 
