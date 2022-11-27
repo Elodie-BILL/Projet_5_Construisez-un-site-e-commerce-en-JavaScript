d6f1d592f1fa3b8dc26ee221b24f6439e65ae040
Elodie B <billelodie@gmail.com>
2022-11-27 13:29:33 +0100

    suppression numéro de commande dans l'url - problème de rechargement de la page

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -8,43 +8,47 @@ function orderNumber(){
         return orderIdNum;
 };
 
-function deleteOrderNumber(){
-    const urlCart = document.location.href;
-    const urlConfirme = new URL(urlCart);
-    const orderIdNum = urlConfirme.searchParams.get("id");
-    
-    console.log(orderIdNum);
-        return orderIdNum;
-};
-
 
 // Afficher numéro de commande 
 let confirmNumber = document.getElementById("orderId");
 console.log(confirmNumber);
 confirmNumber.innerText = `${orderNumber()}`; 
 
+
+//Supprimer numéro de commande
+
+setTimeout(function deleteOrderNumber(){
+    
+    if (orderNumber()){
+        alert("le numéro de commande sera supprimé")
+        document.location = "confirmation.html";
+
+        let id = " supprimé "
+        let confirmNumberCart = document.getElementById("orderId");
+        confirmNumberCart.innerText = `${id}`;
+             
+    
+        
+    };
+   
+},5000 );
+
+// supprimer le reload et information du formulaire
+
 //Suppression panier
 const deleteCart = localStorage.clear('produit');
 console.log(JSON.parse(localStorage.getItem('produit')));
 
 
-// Supression numéro de commande
-// window.location.reload();
-deleteOrderNumber();
 
-// if( closeWindow() ){ 
-//     alert = ('Les informations sur cette page vont être supprimées. Etes-vous de vouloir quittez?');
-//     const urlCart = document.location.href;
-//     console.log(urlCart);
-//     const urlConfirme = new URL(urlCart);
-//     const orderIdNum = '';
-//         return orderIdNum;
 
-// }
 
-// function closeWindow () { 
-//     window.addEventListener("beforeunload", (event) =>{})
-// };  
+
+
+
+
+
+  
 
 
 
